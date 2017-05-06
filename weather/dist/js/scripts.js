cd78c6ba77ee96ededecf0f506587329676d7b63
ehayn <ethanlhayn@gmail.com>
2017-05-06 19:10:40 -0400

    Add files via upload

---
+++ weather/dist/js/scripts.js
@@ -0,0 +1,83 @@
+var weatherCodes = [];
+
+weatherCodes[200] = "Thunderstorm with Light Rain";
+weatherCodes[201] = "Thunderstorm with Rain";
+weatherCodes[202] = "Thunderstorm with Heavy Rain";
+weatherCodes[210] = "Light Thunderstorm";
+weatherCodes[211] = "Thunderstorm";
+weatherCodes[212] = "Heavy Thunderstorm";
+weatherCodes[221] = "Ragged Thunderstorm";
+weatherCodes[230] = "Thunderstorm with Light Drizzle";
+weatherCodes[231] = "Thunderstorm with Drizzle";
+weatherCodes[232] = "Thunderstorm with Heavy Drizzle";
+
+weatherCodes[300] = "Light Intensity Drizzle";
+weatherCodes[301] = "Drizzle";
+weatherCodes[302] = "Heavy Intensity Drizzle";
+weatherCodes[310] = "Light Intensity Drizzle Rain";
+weatherCodes[311] = "Drizzle Rain";
+weatherCodes[312] = "Heavy Intensity Drizzle Rain";
+weatherCodes[313] = "Shower Rain and Drizzle";
+weatherCodes[314] = "Heavy Shower Rain and Drizzle";
+weatherCodes[321] = "Shower Drizzle";
+
+weatherCodes[500] = "Light Rain";
+weatherCodes[501] = "Moderate Rain";
+weatherCodes[502] = "Heavy Intensity Rain";
+weatherCodes[503] = "Very Heavy Rain";
+weatherCodes[504] = "Extreme Rain";
+weatherCodes[511] = "Freezing Rain";
+weatherCodes[520] = "Light Intensity Shower Rain";
+weatherCodes[521] = "Shower Rain";
+weatherCodes[522] = "Heavy Intensity Shower Rain";
+weatherCodes[531] = "Ragged Shower Rain";
+
+weatherCodes[600] = "Light Snow";
+weatherCodes[601] = "Snow";
+weatherCodes[602] = "Heavy Snow";
+weatherCodes[611] = "Sleet";
+weatherCodes[612] = "Shower Sleet";
+weatherCodes[615] = "Light Rain and Snow";
+weatherCodes[616] = "Rain and Snow";
+weatherCodes[620] = "Light Shower Snow";
+weatherCodes[621] = "Shower Snow";
+weatherCodes[622] = "Heavy Shower Snow";
+
+weatherCodes[701] = "Mist";
+weatherCodes[711] = "Smoke";
+weatherCodes[721] = "Haze";
+weatherCodes[731] = "Sand, Dust Whirls";
+weatherCodes[741] = "Fog";
+weatherCodes[751] = "Sand";
+weatherCodes[761] = "Dust";
+weatherCodes[762] = "Volcanic Ash";
+weatherCodes[771] = "Squalls";
+weatherCodes[781] = "Tornado";
+
+weatherCodes[800] = "Clear Sky";
+
+weatherCodes[801] = "Few Clouds";
+weatherCodes[802] = "Scattered Clouds";
+weatherCodes[803] = "Broken Clouds";
+weatherCodes[804] = "Overcast Clouds";
+
+weatherCodes[900] = "Tornado";
+weatherCodes[901] = "Tropical Storm";
+weatherCodes[902] = "Hurricane";
+weatherCodes[903] = "Cold";
+weatherCodes[904] = "Hot";
+weatherCodes[905] = "Windy";
+weatherCodes[906] = "Hail";
+
+weatherCodes[951] = "Calm";
+weatherCodes[952] = "Light Breeze";
+weatherCodes[953] = "Gentle Breeze";
+weatherCodes[954] = "Moderate Breeze";
+weatherCodes[955] = "Fresh Breeze";
+weatherCodes[956] = "Strong Breeze";
+weatherCodes[957] = "High Wind, Near Gale";
+weatherCodes[958] = "Gale";
+weatherCodes[959] = "Severe Gale";
+weatherCodes[960] = "Storm";
+weatherCodes[961] = "Violent Storm";
+weatherCodes[962] = "Hurricane";
